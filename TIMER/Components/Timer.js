031cb5c0311eaba335e325bc9289b6e3e250accc
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-08-26 15:35:12 -0300

    Add files via upload

---
+++ TIMER/Components/Timer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const Timer = () =>{
+    
+    return (
+
+        <div ClassName = "Timer"> 
+    <div ClassName = "container">
+    <div ClassName = "timer_container" >
+        <h1>Timer</h1>
+        <h1>10:30</h1>
+    </div>
+
+    </div>
+    
+    
+    </div>
+    
+    )
+    }
+    
+
+
+
+
+export default Timer
