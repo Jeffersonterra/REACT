348425ec1fe4d013674d9a9f73a6302cc99a7d2d
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-08-26 19:08:33 -0300

    Update Timer.js

--- TIMER/Components/Timer.js
+++ TIMER/Components/Timer.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './timer.css';
+
 const Timer = () =>{
+    const [seconds, setSeconds]= useState(0);
+    const [minutes, setMinutes]= useState(0);
     
+    var timer ;
+    useEffect (()=>{
+        timer = setInterval(()=>{
+            setSeconds(seconds+1);
+            if (seconds ===59){
+                setMinutes (minutes+1);
+                setSeconds (0);
+            }
+        
+        },1000)
+        return ()=>clearInterval (timer);
+    });
     return (
 
         <div ClassName = "Timer"> 
     <div ClassName = "container">
     <div ClassName = "timer_container" >
         <h1>Timer</h1>
-        <h1>10:30</h1>
+        <h1>{minutes} : {seconds}</h1>
     </div>
 
     </div>
