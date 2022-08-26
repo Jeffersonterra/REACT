c7e01673c53599a9cd4fd66b85ed4015613cb782
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-08-26 15:35:51 -0300

    Update App.js

--- TIMER/App.js
+++ TIMER/App.js
@@ -1,29 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome'
-import Firsthello from './components/Firsthello'
-import Hello from './components/Hello'
-import Message from './components/Message'
+
 import Timer from './components/Timer'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
+
 
 function App() {
   return (
     <div className="App">
       <Timer/>
-     {/*
-       <EventBind/>
-      <FunctionClick />
-      <ClassClick/>
-      <Counter/>
-      <Hello/>
-      <Greet/>
-      <Firsthello/>
-  */}
+     
     </div>
   );
 }
