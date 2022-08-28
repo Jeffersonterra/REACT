8e2cfe9ef826d20b0786da9d65f8dc531efac42b
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-08-28 13:12:23 -0300

    Add files via upload

---
+++ JogoDaVelha/scriptmeujogo.js
@@ -0,0 +1,92 @@
+
+
+function clickablediv(){
+    document.getElementById("posicao1").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao1").innerHTML = "X";
+   
+}
+function clickablediv2(){
+    document.getElementById("posicao2").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao2").innerHTML = "X";
+}
+function clickablediv3(){
+    document.getElementById("posicao3").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao3").innerHTML = "X";
+}
+function clickablediv4(){
+    document.getElementById("posicao4").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao4").innerHTML = "X";
+}
+function clickablediv5(){
+    document.getElementById("posicao5").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao5").innerHTML = "X";
+}
+function clickablediv6(){
+    document.getElementById("posicao6").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao6").innerHTML = "X";
+}
+function clickablediv7(){
+    document.getElementById("posicao7").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao7").innerHTML = "X";
+}
+
+function clickablediv8(){
+    document.getElementById("posicao8").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao8").innerHTML = "X";
+}
+function clickablediv9(){
+    document.getElementById("posicao9").style.backgroundColor = "#4169E1";
+    document.getElementById("posicao9").innerHTML = "X";
+}
+
+function doubleclickdiv (){ 
+    document.getElementById("posicao1").style.backgroundColor = "skyblue";
+    document.getElementById("posicao1").innerHTML = "O"; 
+} 
+function doubleclickdiv2(){
+    document.getElementById("posicao2").style.backgroundColor = "skyblue";
+    document.getElementById("posicao2").innerHTML = "O";
+   
+}
+
+function doubleclickdiv3 (){ 
+    document.getElementById("posicao3").style.backgroundColor = "skyblue";
+    document.getElementById("posicao3").innerHTML = "O"; 
+}  
+function doubleclickdiv4(){
+    document.getElementById("posicao4").style.backgroundColor = "skyblue";
+    document.getElementById("posicao4").innerHTML = "O";
+   
+}
+function doubleclickdiv5 (){ 
+    document.getElementById("posicao5").style.backgroundColor = "skyblue";
+    document.getElementById("posicao5").innerHTML = "O"; 
+}
+function doubleclickdiv6(){
+    document.getElementById("posicao6").style.backgroundColor = "skyblue";
+    document.getElementById("posicao6").innerHTML = "O";
+   
+}
+function doubleclickdiv7(){
+    document.getElementById("posicao7").style.backgroundColor = "skyblue";
+    document.getElementById("posicao7").innerHTML = "O";
+   
+}
+function doubleclickdiv8 (){ 
+    document.getElementById("posicao8").style.backgroundColor = "skyblue";
+    document.getElementById("posicao8").innerHTML = "O"; 
+}
+function doubleclickdiv9(){
+    document.getElementById("posicao9").style.backgroundColor = "skyblue";
+    document.getElementById("posicao9").innerHTML = "O";
+   
+}
+
+function vitoriaX(){
+   
+   
+}
+function vitoriaO(){
+   
+   
+}
