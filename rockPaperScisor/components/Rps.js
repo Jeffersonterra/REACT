a4b0a4fa61738668bc277b8227ec458d75c0c626
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-08-30 19:04:45 -0300

    Add files via upload

---
+++ rockPaperScisor/components/Rps.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Rps extends Component {
+  render() {
+    return (<div className="Rps" >
+
+      <div className= "Rock" >Rock</div>
+      <div className= "Paper">Paper</div>
+      <div className= "Scisor" >Scisor</div>
+    </div>
+    )
+  }
+}
+
+export default Rps
