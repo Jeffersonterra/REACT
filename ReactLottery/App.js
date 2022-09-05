cf275487218965fd5547c5ec67b58ca50af5352d
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-09-05 18:29:45 -0300

    Add files via upload

---
+++ ReactLottery/App.js
@@ -0,0 +1,116 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [Lot1, setLot1]= useState(0)
+  const [Lot2, setLot2]= useState(0)
+  const [Lot3, setLot3]= useState(0)
+  const [Lot4, setLot4]= useState(0)
+  const [Lot5, setLot5]= useState(0)
+  const [Lot6, setLot6]= useState(0)
+  const [Lot7, setLot7]= useState(0)
+  const [Lot8, setLot8]= useState(0)
+  const [Lot9, setLot9]= useState(0)
+  const [Lot10, setLot10]= useState(0)
+  const [mensagem, setMensagem]= useState('Ainda nao temos um ganhador')
+
+function girar(){
+  const value = Math.floor(Math.random() * 101);
+  console.log(value);
+  setLot1(value)
+  const value2 = Math.floor(Math.random() * 101);
+  console.log(value2);
+  setLot2(value2)
+  const value3 = Math.floor(Math.random() * 101);
+  console.log(value3);
+  setLot3(value3)
+  const value4 = Math.floor(Math.random() * 101);
+  console.log(value4);
+  setLot4(value4)
+  const value5 = Math.floor(Math.random() * 101);
+  console.log(value5);
+  setLot5(value5)
+  const value6 = Math.floor(Math.random() * 101);
+  console.log(value6);
+  setLot6(value6)
+  const value7 = Math.floor(Math.random() * 101);
+  console.log(value7);
+  setLot7(value7)
+  const value8 = Math.floor(Math.random() * 101);
+  console.log(value8);
+  setLot8(value8)
+  const value9 = Math.floor(Math.random() * 101);
+  console.log(value9);
+  setLot9(value9)
+  const value10 = Math.floor(Math.random() * 101);
+  console.log(value10);
+  setLot10(value10)
+  var input_value = document.getElementById("input").value;
+  console.log(input_value)
+  if (input_value != value ) {
+    setMensagem('Tente novamente!')
+  }
+  if (input_value == value ) {
+   console.log('Voce Ganhou!!')
+   setMensagem('Voce ganhou!!!')
+}
+if (input_value == value2 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+
+if (input_value == value3 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+if (input_value == value4 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+if (input_value == value5 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+if (input_value == value6 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+if (input_value == value7 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+if (input_value == value8 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+if (input_value == value9 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+if (input_value == value10 ) {
+  console.log('Voce Ganhou!!')
+  setMensagem('Voce ganhou!!!')
+}
+}
+
+  return (
+    <div className="App">
+
+    <div className="lot1">{Lot1}</div><div className="lot2">{Lot2}</div><div className="lot3">{Lot3}</div>
+    <div className="lot4">{Lot4}</div><div className="lot5">{Lot5}</div><div className="lot6">{Lot6}</div>
+    <div className="lot7">{Lot7}</div><div className="lot8">{Lot8}</div>
+    <div className="lot9">{Lot9}</div><div className="lot10">{Lot10}</div>
+    <button className="btn" onClick = {girar}> Tente a sorte</button>
+    <div>{mensagem}</div>
+    <input id= "lottoNumber"></input>
+    <div>Your Balance is $500,00</div>
+      <div className = "money">
+
+        <input id= "deposit"></input>
+        <button>Deposit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
